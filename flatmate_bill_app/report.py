287c1179b0742a8f4ea3c01c9cddf3dd779c1cba
Akyas <tu.akyas@gmail.com>
2022-09-22 15:34:13 +0800

    Initial commit and FlatmatesApp-1

---
+++ flatmate_bill_app/report.py
@@ -0,0 +1,46 @@
+import os
+import webbrowser
+
+from fpdf import FPDF
+
+
+class PdfReport:
+    """
+    Creates a PDF file that contains data of the flatmates and the bill
+    """
+
+    def __init__(self, filename):
+        self.filename = filename
+
+    def generate(self, flatmate1, flatmate2, bill):
+        flatmate1_pay = str(round(flatmate1.pays(bill, shared_flatmate=flatmate2), 2))
+        flatmate2_pay = str(round(flatmate2.pays(bill, shared_flatmate=flatmate1), 2))
+
+        pdf = FPDF(orientation='p', unit='pt', format='A4')
+        pdf.add_page()
+
+        # Add house.png icon
+        pdf.image("files/house.png", w=50, h=50)
+
+        # Insert title
+        pdf.set_font(family='Times', size=24, style='B')
+        pdf.cell(w=0, h=80, txt="Flatmates Bill", border=0, align='c', ln=1)
+
+        # Insert Period label and value
+        pdf.set_font(family='Times', size=14, style='B')
+        pdf.cell(w=100, h=40, txt="Period: ", border=0)
+        pdf.cell(w=150, h=40, txt=bill.period, border=0, ln=1)
+
+        # Insert name and due amount of first flatmate
+        pdf.set_font(family='Times', size=12)
+        pdf.cell(w=100, h=25, txt=flatmate1.name, border=0)
+        pdf.cell(w=150, h=25, txt=flatmate1_pay, border=0, ln=1)
+
+        # Insert name and due amount of Second flatmate
+        pdf.cell(w=100, h=25, txt=flatmate2.name, border=0)
+        pdf.cell(w=150, h=25, txt=flatmate2_pay, border=0, ln=1)
+
+        # change directory to files, then generate and open PDF
+        os.chdir('files/')
+        pdf.output(self.filename)
+        webbrowser.open('file://' + os.path.realpath(self.filename))
---
+++ geometry_app/README.md
@@ -0,0 +1,16 @@
+# About
+This is an application that randomly generate a rectangle and gives the coordinates of diagonal edges of the rectangle. 
+The user should guess a point that lies inside the generated rectangle with coordinate information available to him/her.
+Finally, the program says if the guess was right or not.
+
+After the guessing is completed, Rectangle and the points are drawn using Python's turtle library
+
+
+##Credits
+Based on the learnings from https://www.udemy.com/course/the-python-pro-course/learn/lecture/26701418
+
+
+# Topics Learned
+- Python Classes (Object Oriented Programming)
+- Object Creation
+- Inheritence (is-a relationship)
